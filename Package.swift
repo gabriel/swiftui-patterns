691467391c105e026497b4190ac3ed92e89ba2e4
Gabriel Handford <gabrielh@gmail.com>
2025-11-09 12:05:34 -0800

    Create Package

---
+++ Package.swift
@@ -0,0 +1,40 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftUIPatterns",
+    platforms: [
+        .iOS(.v17),
+        .macOS(.v15),
+        .tvOS(.v18),
+        .watchOS(.v11),
+        .visionOS(.v2)
+    ],
+    products: [
+        .library(
+            name: "SwiftUIPatterns",
+            targets: ["SwiftUIPatterns"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/gabriel/swiftui-snapshot-testing", from: "0.1.10")
+    ],
+    targets: [
+        .target(
+            name: "SwiftUIPatterns",
+            dependencies: [],
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "SwiftUIPatternsTests",
+            dependencies: [
+                "SwiftUIPatterns",
+                .product(name: "SwiftUISnapshotTesting", package: "swiftui-snapshot-testing")
+            ],
+            path: "Tests",
+            exclude: ["__Snapshots__"]
+        )
+    ]
+)
